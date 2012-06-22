8f3fa4274dc0f675715b0b94171b4f45f56b9f72
Eric Allen <eric.lee.allen@gmail.com>
2012-06-22 16:26:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timelines::Application.config.secret_token = 'b9ee55d035a14d898793bd011348da3c98bc0bd49d777dbd87eb9e2721c4ab1263886ea08bff99058281fce69c18d1e93dcfe49400e7b88f4dc09f22ed290c31'
