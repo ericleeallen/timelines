c2ab332013d922974d22dd2c1f0cda8056351d51
Eric Allen <eric.lee.allen@gmail.com>
2012-06-24 20:34:54 -0600

    Finish user edit, update, index and destroy

--- db/migrate/20120623020746_create_users.rb
+++ db/migrate/20120623020746_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.date :birth_date
+      t.string 	:name
+      t.string 	:email
+      t.date 	:birth_date
 
       t.timestamps
     end
